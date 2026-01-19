3083e3b47df487ebfb8b6f990456478b996f01fb
vnvss-0x <anassmakhlo@gmail.com>
2026-01-20 00:11:21 +0100

    added missing screenshots

--- src/EXCEPTIONS/exception_ex1/Calculator.java
+++ src/EXCEPTIONS/exception_ex1/Calculator.java
@@ -1,6 +1,7 @@
 package EXCEPTIONS.exception_ex1;
 
 public class Calculator {
+
     public int divide(int a, int b) {
         try {
             return a / b;
@@ -19,9 +20,31 @@ public class Calculator {
         }
     }
 
-    public void calculate(String op, int a, int b) {
-        if (op.equals("/")) divide(a, b);
-        else if (!op.equals("+") && !op.equals("-") && !op.equals("*"))
-            System.out.println("Error: Operation '" + op + "' not supported");
+    public void calculate(String operation, int a, int b) {
+        if (operation.equals("+")) {
+            System.out.println("Result: " + (a + b));
+        } else if (operation.equals("-")) {
+            System.out.println("Result: " + (a - b));
+        } else if (operation.equals("*")) {
+            System.out.println("Result: " + (a * b));
+        } else if (operation.equals("/")) {
+            System.out.println("Result: " + divide(a, b));
+        } else {
+            System.out.println("Error: Operation '" + operation + "' not supported");
+        }
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("--- Testing Division ---");
+        calc.divide(10, 0);
+
+        System.out.println("\n--- Testing Conversion ---");
+        calc.convertToNumber("abc");
+
+        System.out.println("\n--- Testing Operations ---");
+        calc.calculate("&", 5, 5);
+        calc.calculate("+", 10, 5);
     }
 }
